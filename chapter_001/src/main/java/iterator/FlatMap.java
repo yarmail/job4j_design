e9af35071a5eaa715adfe595e0748868fbfcbb88
yarmail <yarmail@yandex.ru>
2020-06-25 21:03:49 +0300

    FlatMap update

---
+++ chapter_001/src/main/java/iterator/FlatMap.java
@@ -0,0 +1,59 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+
+/**
+ * Get another stream from an iterator stream item
+ * Получить из элемента потока итераторов другой поток
+ * 5.1.4. Создать convert(Iterator<Iterator>) [#293771]
+ */
+
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> it;
+    private T t;
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        while (data.hasNext()) {
+            while (data.next().hasNext()) {
+
+            }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return it.next();
+    }
+}
+
+
+/* так делать нельзя
+
+    private List<T> temp = new ArrayList<>();
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+        while (data.hasNext()) {
+            Iterator<T> inner = data.next();
+            while (inner.hasNext()) {
+                temp.add(inner.next());
+            }
+        }
+    }
+*/
