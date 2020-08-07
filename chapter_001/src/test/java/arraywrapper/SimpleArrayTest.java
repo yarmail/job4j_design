e6ca38dbf0beee0b1db2242408a845eca7bd9079
yarmail <yarmail@yandex.ru>
2020-08-07 21:24:53 +0300

    1. Динамический список на массиве. [#293763] Update

--- chapter_001/src/test/java/arraywrapper/SimpleArrayTest.java
+++ chapter_001/src/test/java/arraywrapper/SimpleArrayTest.java
@@ -10,9 +10,10 @@ import java.util.NoSuchElementException;
 
 public class SimpleArrayTest {
 
+    private SimpleArray<String> array = new SimpleArray<>();
+
     @Test
     public void whenAddThenGet() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         String rsl = array.get(0);
         assertThat(rsl, is("first"));
@@ -20,7 +21,6 @@ public class SimpleArrayTest {
 
     @Test
     public void whenAddThenIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         String rsl = array.iterator().next();
         assertThat(rsl, is("first"));
@@ -28,26 +28,22 @@ public class SimpleArrayTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetEmpty() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.get(0);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetOutBound() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         array.get(1);
     }
 
     @Test(expected = NoSuchElementException.class)
     public void whenGetEmptyFromIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.iterator().next();
     }
 
     @Test(expected = ConcurrentModificationException.class)
     public void whenCorruptedIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         Iterator<String> it = array.iterator();
         array.add("second");
