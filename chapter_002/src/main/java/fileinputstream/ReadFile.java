ce93b810652eb7f5bf3264f67d50e99a7a3e058c
yarmail <yarmail@yandex.ru>
2020-09-27 19:04:48 +0300

    0.2. FileInputStream [#293819]

---
+++ chapter_002/src/main/java/fileinputstream/ReadFile.java
@@ -0,0 +1,33 @@
+package fileinputstream;
+
+import java.io.FileInputStream;
+
+/**
+ * Конструкция
+ * Данные считываются по байтам.
+ * Здесь используется конструкция try-with-resources,
+ * чтобы закрыть поток ввода.
+ */
+
+public class ReadFile {
+
+    /**
+     * Получившийся текст можно разить на строчки через метод String.split.
+     * String[] lines = text.toString().split(System.lineSeparator());
+     * for (String line : lines) {
+     *     System.out.println(line);
+     * }
+     */
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != 1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_002/src/main/java/fileinputstream/package-info.java
@@ -0,0 +1,19 @@
+package fileinputstream;
+
+/**
+ *
+ * 0.2. FileInputStream [#293819]
+ * В этом уроке мы узнаем, как в Java прочитать файл.
+ *
+ * java.io.FileInputStream
+ * Этот класс позволяет прочитать данные из файла.
+ *
+ * Создайте файл в корне проекта. Дайте ему имя input.txt.
+ * Вставьте в него текст.
+ *
+ * login=login
+ * password=password
+ * url=url
+ *
+ *
+ */
