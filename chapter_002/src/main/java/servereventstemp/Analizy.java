23b5f953cb57d791750214aba856b5c7f437ec32
yarmail <yarmail@yandex.ru>
2020-10-08 13:47:02 +0300

    3.0. Тестирование IO [#293809] update

--- chapter_002/src/main/java/servereventstemp/Analizy.java
+++ chapter_002/src/main/java/servereventstemp/Analizy.java
@@ -17,14 +17,13 @@ public class Analizy {
         toList(source);
         String start = null;
         String end = null;
-        String resultStr = null;
         for (String line : tmpList) {
             if ((line.contains("400") || line.contains("500")) && start == null) {
                 start = line.substring(4, line.length());
             }
             if ((line.contains("200") || line.contains("300")) && (start != null && end == null)) {
                 end = line.substring(4, line.length());
-                resultStr = start + ";" + end;
+                String resultStr = start + ";" + end;
                 start = null;
                 end = null;
                 resultList.add(resultStr);
@@ -45,8 +44,7 @@ public class Analizy {
     private static void toFile(String target) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
+                        new FileOutputStream(target)))) {
             resultList.forEach(value -> out.write(value + System.lineSeparator()));
         } catch (Exception e) {
             e.printStackTrace();
--- chapter_002/src/test/java/servereventstemp/AnalizyTest.java
+++ chapter_002/src/test/java/servereventstemp/AnalizyTest.java
@@ -1,7 +1,7 @@
 package servereventstemp;
 
-import java.util.List;
 import java.io.*;
+import java.util.StringJoiner;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,29 +19,32 @@ public class AnalizyTest {
     public void unavailableTest() throws IOException {
         File source = tmpFolder.newFile("source.txt");
         File target = tmpFolder.newFile("target.txt");
+        String ls = System.lineSeparator();
         try (PrintWriter out = new PrintWriter(source)) {
-            out.write("200 10:56:01\n" +
-                         "500 10:57:01\n" +
-                         "400 10:58:01\n" +
-                         "200 10:59:01\n" +
-                         "500 11:01:02\n" +
-                         "200 11:02:02");
+            out.write("200 10:56:01"
+                    + ls
+                    +    "500 10:57:01"
+                    + ls
+                    +    "400 10:58:01"
+                    + ls
+                    +    "200 10:59:01"
+                    + ls
+                    +    "500 11:01:02"
+                    + ls
+                    +    "200 11:02:02");
         }
         // Кажется интересным то, что в метод вместо файла подставляется String путь к этому файлу
         Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
 
         // target переводим в строку для проверки результатов
-        StringBuilder rsl = new StringBuilder();
-        try(BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(rsl::append);
+        // НЕ ЗАБЫВАЕМ К КАЖДОЙ СЧИТЫВАЕМОЙ СТРОКЕ ДОБАВЛЯТЬ РАЗДЕЛИТЕЛЬ
+        StringJoiner rsl = new StringJoiner(System.lineSeparator());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::add);
         }
         assertThat(rsl.toString(), is(
-                "10:57:01;10:59:01\n" +
-                      "11:01:02;11:02:02"));
-
-
+                "10:57:01;10:59:01"
+                  + ls
+                  +   "11:01:02;11:02:02"));
     }
-
-
-
 }
