b43c11932f2e5c571cb35b6ef71da20f5282c677
yarmail <yarmail@yandex.ru>
2020-10-19 22:50:30 +0300

    5.1. Архивировать проект. [#293817]

--- chapter_002/src/main/java/zipproject/ArgZip.java
+++ chapter_002/src/main/java/zipproject/ArgZip.java
@@ -1,24 +1,88 @@
 package zipproject;
 
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * validate according pattern
+ * проверить согласно шаблону
+ *"java -jar pack.jar -d=c:\project\job4j\ -e=class -o=project.zip"
+ * Как я понимаю, в этой строке нам нужно извлечь
+ * следующие параметры:
+ * -d - directory - которую мы хотим архивировать
+ * -e - exclude - исключить файлы *.xml
+ * -o - output - во что мы архивируем - выходной файл
+ */
 public class ArgZip {
     private final String[] args;
+    private Map<String, String> map = new HashMap<>();
+
     public ArgZip(String[] args) {
         this.args = args;
     }
-
+// В этой части класса вычленяем параметры из строки аргументов
+// и проводим некоторые проверки аргументов
+    /**
+     * Является ли путь директорией
+     */
     public boolean valid() {
-        return false;
+        File file = new File(directory());
+        return file.isDirectory();
     }
-
+    /**
+     * Работаем с исходной папкой
+     * Как я понимаю, в нашей задаче,
+     * мы получаем не массив строк аргументов (как в реальности),
+     * а одну строку arg[0] которую, в свою очередь,
+     * нам нужно разбить на массив строк
+     *
+     * Далее, чтобы получить ключ и папку нам нужен 3 элемент массива
+     * -d=c:\project\job4j\
+     * мы разбиваем эту подстроку ещё раз на массив с двумя элементами
+     * (разделитель знак =)
+     * и проверяем, есть ли второй элемент
+     *
+     */
     public String directory() {
-        return null;
+        String[] str = args[3].split("=");
+        if (str[1].isEmpty()) {
+            throw new IllegalArgumentException("Отсутствует путь к исходной папке");
+        }
+        return str[1];
     }
-
+    /**
+     * Работаем с параметром Исключить файлы по расширению
+     * Выполняем операции, аналогичные работе с папкой
+     *
+     */
     public String exclude() {
-        return null;
+        String[] str = args[4].split("=");
+        if (str[1].isEmpty()) {
+            throw new IllegalArgumentException("Отсутствует параметр Исключение файлов по расширению");
+        }
+        return str[1];
     }
-
+    /**
+     * Аналогично работаем с параметром выходного файла
+     *
+     */
     public String output() {
-        return null;
+        String[] str = args[5].split("=");
+        if (str[1].isEmpty()) {
+            throw new IllegalArgumentException("Имя выходного файла не задано");
+        }
+        return str[1];
+    }
+// В этой части класса как в задании Именованные аргументы
+// заполняем мапу и добавляем методы для работы с ней
+    public void setValue() {
+        map.put("d", directory());
+        map.put("e", exclude());
+        map.put("o", output());
+    }
+    public String getValue(String key) {
+        return map.get(key);
     }
 }
--- chapter_002/src/main/java/zipproject/Zip.java
+++ chapter_002/src/main/java/zipproject/Zip.java
@@ -1,16 +1,36 @@
 package zipproject;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
-
+    /**
+     * Метод для упаковки папки
+     * Архив должен сохранять структуру проекта.
+     * То есть содержать подпапки.
+     */
+    public void packFiles(List<Path> sources, File target) throws IOException {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            Iterator<Path> it = sources.iterator();
+            while (it.hasNext()) {
+                zip.putNextEntry(new ZipEntry(it.next().toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(it.next().toFile()))) {
+                    zip.write(out.readAllBytes());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-
+    /**
+     * Пример метод для упаковки одного файла
+     */
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
@@ -22,10 +42,30 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * В качестве параметров запуска я выбрал следующую строку
+     * "java -jar pack.jar -d=./chapter_002 -e=txt -o=./chapter_002/src/main/java/zipproject/chapter_002.zip"
+     * Сответственно у меня должно получиться:
+     * args[0] = "java"
+     * args[1] = "-jar"
+     * args[2] = "pack.jar"
+     * args[3] = "-d=./chapter_002"
+     * args[4] = "-e=txt"
+     * args[5] = "-o=./chapter_002/src/main/java/zipproject/chapter_002.zip"
+     *
+     */
+    public static void main(String[] args) throws IOException {
+
+/* пример упаковки одного файла
         new Zip().packSingleFile(
                 new File("./chapter_002/pom.xml"),
                 new File("./chapter_002/pom.zip")
         );
+*/
+
+    ArgZip argZip = new ArgZip(args);
+    argZip.setValue();
+    List<Path> paths = SimpleSearch.search(Paths.get(argZip.getValue("d")), argZip.getValue("e"));
+    new Zip().packFiles(paths, new File(argZip.getValue("o")));
     }
 }
