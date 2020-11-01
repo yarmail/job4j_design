7c1a0bac8ae0bce5803057f2c21404dd9ca24445
yarmail <yarmail@yandex.ru>
2020-11-01 12:39:15 +0300

    Log4j. Логирование системы. [#293803]

---
+++ chapter_002/src/main/java/log/UsageLog4j.java
@@ -0,0 +1,18 @@
+package log;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class UsageLog4j {
+
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+
+}
