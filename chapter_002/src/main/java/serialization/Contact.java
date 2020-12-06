8a810a36063b863fedee454b0bfb03117effc5b8
yarmail <yarmail@yandex.ru>
2020-12-06 22:17:25 +0300

    1. Что такое Сериализация? [#313163]

---
+++ chapter_002/src/main/java/serialization/Contact.java
@@ -0,0 +1,59 @@
+package serialization;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int zipCode;
+    private final String phone;
+
+
+    public Contact(int zipcode, String phone) {
+        this.zipCode = zipcode;
+        this.phone = phone;
+    }
+
+    public int getZipcode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode = " + zipCode
+                + ", phone='" + phone + '\''
+                + '}';
+    }
+
+    /**
+     * Проверить создание файла можно
+     * по адресу:
+     * C:\Users\USERNAME\AppData\Local\Temp
+     * или через debug
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
+
+        /* Запись объекта в файл */
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos =
+                     new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+
+        /* Чтение объекта из файла */
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois =
+                     new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println(contactFromFile);
+        }
+        tempFile.delete();
+    }
+}
