94c7f39b415c5c3eb782c7156f16c27e88e5e433
yarmail <yarmail@yandex.ru>
2020-12-27 16:02:10 +0300

    Cимулировать поведение команды cd update

--- chapter_002/src/main/java/cd/Shell.java
+++ chapter_002/src/main/java/cd/Shell.java
@@ -1,14 +1,25 @@
 package cd;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Shell {
-    String result = "";
+    private String result = "";
+
+    private void normalize(String path) {
+        Path pathToPath = Paths.get(path).normalize();
+        result = pathToPath.toString().replaceAll(".*[/\\\\].*", "/");
+    }
 
     public void cd(String path) {
         if (path.equals("/")) {
             result = "/";
-        } else if (path.contains("..")) {
-            result = "/";
-        } else {
+        } else if (result.equals("") && path.contains("..")) {
+            normalize(path);
+        } else if (!result.equals("") && path.contains("..")) {
+            path = result + "/" + path;
+            normalize(path);
+        } else  {
             result = result + "/" + path;
         }
     }
