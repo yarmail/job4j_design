9669bd5969fb3908be4037279461bf9b40bf38f3
yarmail <yarmail@yandex.ru>
2021-02-09 00:38:55 +0300

    Фильтры [#293746] update

--- chapter_003/src/main/java/filters/process.sql
+++ chapter_003/src/main/java/filters/process.sql
@@ -10,7 +10,6 @@ id serial primary key,
 name varchar(255),
 expired_date timestamp,
 price money,
-amount int, -- количество продукта
 type_id int references type(id)
 );
 
@@ -19,12 +18,12 @@ insert into type values (2, 'Молоко');
 insert into type values (3, 'Мясо');
 insert into type values (4, 'Хлеб');
 
-insert into product values (1, 'сыр костромской', date '2021-01-01', 100.00, 99, 1);
-insert into product values (2, 'сыр рязанский', date '2021-02-01', 200.00, 98, 1);
-insert into product values (3, 'мороженое ялта', date '2021-03-01', 300.00, 97, 2);
-insert into product values (4, 'восторг мороженое', date '2021-03-01', 400.00, 9, 2);
-insert into product values (5, 'мясо свиное', date '2021-04-01', 500.00, 8, 3);
-insert into product values (6, 'хлеб дарницкий', date '2021-05-01', 600.00, 7, 4);
+insert into product values (1, 'сыр костромской', date '2021-01-01', 100.00, 1);
+insert into product values (2, 'сыр рязанский', date '2021-02-01', 200.00, 1);
+insert into product values (3, 'мороженое ялта', date '2021-03-01', 300.00, 2);
+insert into product values (4, 'восторг мороженое', date '2021-03-01', 400.00, 2);
+insert into product values (5, 'мясо свиное', date '2021-04-01', 500.00, 3);
+insert into product values (6, 'хлеб дарницкий', date '2021-05-01', 600.00, 4);
 
 -- Задание.
 
@@ -40,16 +39,21 @@ where name LIKE '%мороженое%';
 
 -- 3. Написать запрос, который выводит все продукты,
 -- срок годности которых заканчивается в следующем месяце.
-select product.name from product
-where expired_date > current_date
-and   expired_date < current_date + interval '1 month';
+-- Как я понял вопрос - если на данный момент февраль - то какие в марте.
+
+select name from product
+where extract(month from expired_date) = extract(month from now() + interval '1 month');
 
 -- 4. Написать запрос, который выводит самый дорогой продукт.
-select max(price) from product;
+select * from product
+order by price desc limit 1;
 
 -- 5. Написать запрос, который выводит количество
 -- всех продуктов определенного типа.
-select name, amount from product;
+select type.name, count(type.name)
+from product inner join type
+on product.type_id = type.id
+group by type.name;
 
 -- 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
 select product.name
