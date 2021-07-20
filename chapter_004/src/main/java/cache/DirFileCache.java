358c78ebc4e50e38cfef0ab0a4fb00385c078647
yarmail <yarmail@yandex.ru>
2021-07-21 00:24:30 +0300

    1. Реализация кеша на SoftReference [#1592 #178798]

---
+++ chapter_004/src/main/java/cache/DirFileCache.java
@@ -0,0 +1,41 @@
+package cache;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Создать программу, эмулирующую поведение данного кеша.
+ * Программа должна считывать текстовые файлы из системы
+ * и выдавать текст при запросе имени файла.
+ * Если в кеше файла нет, кеш должен загрузить себе данные.
+ * По умолчанию в кеше нет ни одного файла.
+ * Текстовые файл должны лежать в одной директории.
+ * Пример. Names.txt, Address.txt - файлы в системе.
+ * При запросе по ключу Names.txt -
+ * кеш должен вернуть содержимое файла Names.txt.
+ */
+public class DirFileCache extends AbstractCache <String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+/**
+ * java 11
+ * Path.of(cachingDir, key) - превращается в путь к файлу
+ *
+ */
+    @Override
+    protected String load(String key) {
+        String result = null;
+        try {
+            result = Files.readString(Path.of(cachingDir, key));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ chapter_004/src/main/java/cache/Emulator.java
@@ -0,0 +1,29 @@
+package cache;
+
+import java.lang.ref.SoftReference;
+import java.nio.file.Path;
+
+/**
+ * Создать класс Emulator для работы с пользователем.
+ * Предоставить пользователю возможности:
+ * - указать кэшируемую директорию
+ * - загрузить содержимое файла в кэш (получить в мапе мягкую ссылку?)
+ * - получить содержимое файла из кэша
+ *
+ * Короче это тестирование других  классов
+ */
+public class Emulator {
+
+    public static void main(String[] args) {
+        // задаем папку для хранения файлов в которой уже находятся наши контрольные файлы
+        String dir = Path.of("chapter_004","src", "main", "resources").toString();
+        DirFileCache dirFileCache = new DirFileCache(dir);
+        //Предположим user выбирает файл Address.txt. Получаем мягкую ссылку от нужного файла и заносим её в мапу
+        dirFileCache.get("Address.txt");
+        //Получаем из мапы мягкую ссылку
+        SoftReference <String> softReference = dirFileCache.cache.get("Address.txt");
+        //пробуем по мягкой ссылке получить текст файла
+        System.out.println(softReference.get());
+        //вроде бы все получилось хорошо
+    }
+}
