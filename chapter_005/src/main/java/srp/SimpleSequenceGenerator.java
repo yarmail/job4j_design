581147560873a9bf238d8a8c6fc764dc48e4226f
yarmail <yarmail@yandex.ru>
2021-10-04 22:10:38 +0300

    0. Принцип единственной ответственности [#4913]

--- chapter_005/src/main/java/srp/SimpleSequenceGenerator.java
+++ chapter_005/src/main/java/srp/SimpleSequenceGenerator.java
@@ -3,14 +3,19 @@ package srp;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.Random;
 
 public class SimpleSequenceGenerator implements SequenceGenerator<Integer> {
+
+    private final NumberGenerator<Integer> numberGenerator;
+
+    public SimpleSequenceGenerator(NumberGenerator<Integer> numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
     @Override
     public List<Integer> generate(int size) {
-        Random random = new Random();
         return IntStream.range(0, size)
-                .map(i -> random.nextInt()).boxed()
+                .map(i -> numberGenerator.generate()).boxed()
                 .collect(Collectors.toList());
     }
 }
