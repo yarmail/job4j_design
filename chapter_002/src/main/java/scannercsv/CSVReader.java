4b5333913b46e2c4444d221306f241fd4edafe77
yarmail <yarmail@yandex.ru>
2022-10-11 15:05:02 +0300

    7. Scanner [#504791 #350417]

--- chapter_002/src/main/java/scannercsv/CSVReader.java
+++ chapter_002/src/main/java/scannercsv/CSVReader.java
@@ -1,98 +1,76 @@
 package scannercsv;
 
 import java.io.*;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+
 
 /**
+ * 7. Scanner [#504791 #350417]
+ * В качестве входных данных задается путь к файлу path,
+ * разделитель delimiter, например: ; ,
+ * приемник данных out: stdout или путь к файлу,
+ * фильтр по столбцам filter, например: name,age
  * (есть тесты)
  */
 public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+        String output = argsName.get("out");
+        try (Scanner scanner = new Scanner(new FileInputStream(argsName.get("path")));
+             PrintStream out = "stdout".equals(output) ? new PrintStream(System.out)
+                     : new PrintStream(new FileOutputStream(output))) {
+            scanner.useDelimiter(System.lineSeparator());
+            List<String> filters = Arrays.stream(argsName.get("filter").split(","))
+                    .collect(Collectors.toList());
+            List<String> columns = Arrays.stream(scanner.next().split(argsName.get("delimiter")))
+                    .collect(Collectors.toList());
+            List<Integer> indexes = filters.stream()
+                    .mapToInt(columns::indexOf)
+                    .filter(index -> index != -1)
+                    .boxed()
+                    .collect(Collectors.toList());
 
-    public static void handle(ArgsName argsName) {
-        List<List<String>> lines = read(argsName);
-        List<Integer> ind =  index(lines, argsName.get("filter").split(","));
-        if ("stdout".equals(argsName.get("out"))) {
-            console(lines, ind, argsName);
-        } else {
-            write(lines, ind, argsName);
-        }
-    }
-
-    public static List<Integer> index(List<List<String>> lines, String[] filter) {
-        List<Integer> in = new ArrayList<>();
-        for (String f : filter) {
-            in.add(lines.get(0).indexOf(f));
-        }
-        return in;
-    }
-
-    public static List<List<String>> read(ArgsName argsName) {
-        List<List<String>> lines = new ArrayList<>();
-        try (var scanner = new Scanner(Path.of(argsName.get("path"))).useDelimiter(argsName.get("delimiter"))) {
+            String filteredColumns = filterLine(columns, indexes, argsName.get("delimiter"));
+            out.println(filteredColumns);
             while (scanner.hasNext()) {
-                lines.add(Arrays.asList(scanner.nextLine().split(argsName.get("delimiter"))));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lines;
-    }
-
-    public static void console(List<List<String>> lines, List<Integer> in, ArgsName argsName) {
-        for (List<String> l :lines) {
-            for (int i = 0; i < in.size(); i++) {
-                if (i < in.size() - 1) {
-                    System.out.print(l.get(in.get(i)) + ";");
-                } else {
-                    System.out.print(l.get(in.get(i)));
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void write(List<List<String>> lines, List<Integer> in, ArgsName argsName) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(argsName.get("out"), true))) {
-            for (List<String> l :lines) {
-                for (int i = 0; i < in.size(); i++) {
-                    if (i < in.size() - 1) {
-                        pw.print(l.get(in.get(i)) + ";");
-                    } else {
-                        pw.print(l.get(in.get(i)));
-                    }
-                }
-                pw.println();
+                List<String> line = Arrays.asList(scanner.next().split(argsName.get("delimiter")));
+                String filteredLine = filterLine(line, indexes, argsName.get("delimiter"));
+                out.println(filteredLine);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void check(ArgsName argsName) {
-        if (!argsName.get("path").endsWith(".csv")) {
-            throw new IllegalArgumentException("No read File!");
-        }
-        if (!";".equals(argsName.get("delimiter"))) {
-            throw new IllegalArgumentException("Does not contain the sing \";\"");
-        }
-        if (!("stdout".equals(argsName.get("out")) || argsName.get("out").endsWith(".csv"))) {
-            throw new IllegalArgumentException("No write File");
-        }
-        if (!(argsName.get("filter").contains("name")
-                || argsName.get("filter").contains("age")
-                || argsName.get("filter").contains("last_name")
-                || argsName.get("filter").contains("education"))) {
-            throw new IllegalArgumentException("No filter");
-        }
+    private static String filterLine(List<String> line, List<Integer> indexes, String delimiter) {
+        return indexes.stream()
+                .map(line::get)
+                .collect(Collectors.joining(delimiter));
     }
 
-    public static void main(String[] args) throws IOException {
+
+
+    private static boolean checkArgs(ArgsName argsName) {
+        if (!Paths.get(argsName.get("path")).toFile().exists()) {
+            throw new IllegalArgumentException("The file does not exist");
+        }
+        if (argsName.get("out").isEmpty()) {
+            throw new IllegalArgumentException("Out doesn't exist");
+        }
+        if (argsName.get("filter").isBlank()) {
+            throw new IllegalArgumentException("Filter doesn't exist");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
         if (args.length != 4) {
-            throw new IllegalArgumentException("No arguments!");
+            throw new IllegalArgumentException("args.length != 4");
         }
-        ArgsName argsName = ArgsName.of(args);
-        check(argsName);
-        handle(argsName);
+        if (checkArgs(ArgsName.of(args))) {
+            handle(ArgsName.of(args));
+        }
+
     }
 }
