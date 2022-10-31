90c1100ec7d624fb4e4f61f2e9e810a243cbf706
yarmail <yarmail@yandex.ru>
2022-10-31 13:38:09 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ chapter_003/src/main/java/transactionisolation/package-info.java
@@ -0,0 +1,16 @@
+package transactionisolation;
+
+/*
+4.3. Транзакции. Уровни изолированности транзакций. [#504812]
+
+Чтение неподтвержденных данных, грязное чтение
+(read uncommitted, dirty read)
+
+Чтение подтвержденных данных (read committed)
+
+Повторяющееся чтение (repeatable read)
+
+Снимок (snapshot)
+
+Сериализация (serializable)
+ */
